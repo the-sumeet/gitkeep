fa0b8f8fb25f39348c444a6ca4863304dd85f1e2
sumeet <Sumeet.Mathpati@steel-eye.com>
2025-01-12 12:48:04 +0530

    replaced notes path to url instead of query

--- src/routes/list/[...catchall]/+page.ts
+++ src/routes/list/[...catchall]/+page.ts
@@ -1,13 +1,13 @@
 import { browser } from '$app/environment';
 import { goto } from '$app/navigation';
 import { redirect } from '@sveltejs/kit';
-import type { PageLoad } from './$types';
+import type { PageLoad } from '../$types';
 import { getContent } from './utils';
 
 export const load: PageLoad = async ({ url, fetch, parent }) => {
 
-    const path = url.searchParams.get('path') || '';
-
+    // const path = url.searchParams.get('path') || '';
+    const path = url.pathname.replace("/list", "");
     const session = (await parent() as App.ParentData).session;
     if (!session!.repo) {
         if (browser) {
