43a6ab4838c8b07bcee8e32cc9300e3180460961
sumeet <Sumeet.Mathpati@steel-eye.com>
2025-01-12 22:47:11 +0530

    ruse bs icons, and fix page hyperlinks

--- src/routes/list/[...catchall]/Breadcrumb.svelte
+++ src/routes/list/[...catchall]/Breadcrumb.svelte
@@ -1,52 +1,37 @@
 <script lang="ts">
+
 	type PropType = {
 		paths: string[];
 	};
 	let { paths }: PropType = $props();
 
+	function getPathRoute(index: number) {
+		return '/list/' + paths.slice(0, index + 1).join('/');
+	}
+
 </script>
 
-<nav class="flex" aria-label="Breadcrumb">
-	<ol role="list" class="flex flex-wrap items-center space-x-4 space-y-2">
+<nav class="border rounded-md p-2 shadow-md flex items-center" aria-label="Breadcrumb">
+	<ol role="list" class="flex flex-wrap items-center space-x-4 ">
 		<li>
 			<div>
-				<a href="#" class="text-gray-400 hover:text-gray-500">
-					<svg
-						class="size-5 shrink-0"
-						viewBox="0 0 20 20"
-						fill="currentColor"
-						aria-hidden="true"
-						data-slot="icon"
-					>
-						<path
-							fill-rule="evenodd"
-							d="M9.293 2.293a1 1 0 0 1 1.414 0l7 7A1 1 0 0 1 17 11h-1v6a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-3a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-6H3a1 1 0 0 1-.707-1.707l7-7Z"
-							clip-rule="evenodd"
-						/>
-					</svg>
+				<a href="/list" class="text-gray-400 hover:text-gray-500">
+					<i class="text-xl bi bi-house"></i>
 					<span class="sr-only">Home</span>
 				</a>
 			</div>
 		</li>
 		{#if paths.length > 0}
-			{#each paths as path}
+			{#each paths as path, i}
 				<li>
 					<div class="flex items-center">
-						<svg
-							class="size-5 shrink-0 text-gray-400"
-							viewBox="0 0 20 20"
-							fill="currentColor"
-							aria-hidden="true"
-							data-slot="icon"
+						<i class="text-lg bi bi-chevron-right text-gray-500"></i>
+						{#if i === paths.length - 1}
+							<span class="ml-4 text-sm font-medium text-gray-500">{path}</span>
+						{:else}
+						<a href={getPathRoute(i)} class="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700">{path}</a
 						>
-							<path
-								fill-rule="evenodd"
-								d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z"
-								clip-rule="evenodd"
-							/>
-						</svg>
-						<a href="#" class="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700">{path}</a
-						>
+						{/if}
 					</div>
 				</li>
 			{/each}
