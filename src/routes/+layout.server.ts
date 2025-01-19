02a1a3c9f2198dc732840333f3ae996491338f84
sumeet <Sumeet.Mathpati@steel-eye.com>
2025-01-19 19:49:50 +0530

    removed unused param

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -3,11 +3,13 @@ import type { LayoutServerLoad } from './$types';
 import { SKIP_AUTH_PATHS } from '$lib/constants';
 import { parseObjId } from '$lib/db';
 
-export const load: LayoutServerLoad = async ({cookies, url, locals}) => {
+export const load: LayoutServerLoad = async ({ url, locals}) => {
 
 	const session = locals.session;
 	if (!session && !SKIP_AUTH_PATHS.includes(url.pathname) ) {
 		throw redirect(307, '/token');
+	} else if (session && url.pathname === "/") {
+		throw redirect(307, '/list');
 	}
 
 	return {
