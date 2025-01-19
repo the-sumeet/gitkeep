228239f3c66cf56a92561b734a54ff7f776075db
sumeet <Sumeet.Mathpati@steel-eye.com>
2025-01-19 19:55:14 +0530

    formatting

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,10 +1,4 @@
 import type { PageLoad } from './$types';
-import { page } from '$app/state';
-import { state } from '$lib/store.svelte';
-import { goto } from '$app/navigation';
-import { redirect } from '@sveltejs/kit';
-
-
 
 export const load: PageLoad = async ({parent}) => {
 
