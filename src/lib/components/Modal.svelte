5ab327dbf4ebd9f74cfc5859056252e503e556b2
sumeet <Sumeet.Mathpati@steel-eye.com>
2025-01-23 09:53:11 +0530

    changed z index of modal

--- src/lib/components/Modal.svelte
+++ src/lib/components/Modal.svelte
@@ -14,7 +14,7 @@
 
 <div class="fixed inset-0 bg-gray-500/75 transition-opacity" aria-hidden="true"></div>
 
-<div class="fixed inset-0 z-10 w-screen overflow-y-auto">
+<div class="fixed inset-0 z-15 w-screen overflow-y-auto">
 	<div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
 		<!--
 					Modal panel, show/hide based on modal state.
