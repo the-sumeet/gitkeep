ba8e0bad2bcef9cf4f562855b9bc933d2a2bca37
sumeet <Sumeet.Mathpati@steel-eye.com>
2025-01-24 16:48:23 +0530

    use os env var or env var from .env file

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,7 +2,7 @@ import { MongoClient, ServerApiVersion } from 'mongodb';
 import { MONGODB_URI } from '$env/static/private';
 import { MONGODB_NAME } from './constants';
 
-const client = new MongoClient(MONGODB_URI, {
+const client = new MongoClient(process.env.MONGODB_URI || MONGODB_URI, {
     serverApi: {
         version: ServerApiVersion.v1,
         strict: true,
