6d50136964f5084cfca34adeb0e45a86a4f725dc
sumeet <Sumeet.Mathpati@steel-eye.com>
2025-01-28 20:28:56 +0530

    deleting session on logout

--- src/routes/api/logout/+server.ts
+++ src/routes/api/logout/+server.ts
@@ -1,7 +1,15 @@
+import { sessions } from "$lib/collections";
 import { COOKIE_SESSION_KEY_NAME } from "$lib/constants";
 import { json } from "@sveltejs/kit";
 
-export async function POST({ cookies}) {
+export async function POST({ cookies, locals}) {
+
+    const userSession = (locals as App.Locals).session;
+    if (userSession) {
+        await sessions.deleteOne({ _id: userSession!._id });
+    }
+
     cookies.delete(COOKIE_SESSION_KEY_NAME, { path: '/' });
+
     return json({ message: 'logged out' }, { status: 200 });
 }
